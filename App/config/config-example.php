be6136d052fb1642f4c47ef8306096586f96d557
Evaldas Vaitonis <ritmas@Evaldass-iMac.local>
2019-02-18 17:42:50 +0200

    config file constants, database class methods and connection to database using PDO

---
+++ App/config/config-example.php
@@ -0,0 +1,10 @@
+<?php
+
+define('DB_HOST', 'your_db_host');
+define('DB_USER', 'your_db_user');
+define('DB_PASS', 'your_db_password');
+define('DB_NAME', 'your_db_name');
+
+define('APPROOT',dirname(dirname(__FILE__ )));
+define('URLROOT', 'Your url root, eg. http://localhost/hairsalon ');
+define('SITENAME', 'your_site_name');
--- App/config/config.php
+++ App/config/config.php
@@ -1,7 +1,13 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: ritmas
- * Date: 18/02/2019
- * Time: 16:36
- */
+
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', 'mysql');
+define('DB_NAME', 'hairsalon');
+
+//APP root
+define('APPROOT',dirname(dirname(__FILE__ )));
+//URL root
+define('URLROOT', 'http://localhost/hairsalon-booking-mvc');
+// Site Name
+define('SITENAME', 'Kirpyklos rezarvacijų sistema');
